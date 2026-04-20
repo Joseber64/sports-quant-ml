f006dd21ea52ef209e5cf8f09483307ec9475dff
Quant-Ing <luisenriqueramirez185@gmail.com>
2026-04-20 16:36:37 -0600

    Lumina Strategic Patch: Se mejora el contrato del repositorio para que las operaciones de actualización y eliminación retornen el recurso o la confirmación de forma más descriptiva.

--- src/interfaces/IRepository.ts
+++ src/interfaces/IRepository.ts
@@ -1,7 +1,7 @@
 export interface IRepository<T> {
   findById(id: string): Promise<T | null>;
   findAll(): Promise<T[]>;
-  create(item: T): Promise<T>;
-  update(id: string, item: Partial<T>): Promise<boolean>;
+  create(item: Omit<T, 'id'> | T): Promise<T>;
+  update(id: string, item: Partial<T>): Promise<T | null>;
   delete(id: string): Promise<boolean>;
 }
